6655c1ef51cca8a0191362bcb738b61c271cd917
Cloud <minhkjl3@gmail.com>
2018-07-28 12:29:51 +0700

    sound handler and constants

---
+++ constants.py
@@ -0,0 +1,6 @@
+import pyaudio
+
+USER_ID = 'usr_f1865f5c17b047b18a29686099d7fa45'
+CHUNK_SIZE = 320
+RATE = 16000
+FORMAT = pyaudio.paInt16
---
+++ sound_handler.py
@@ -0,0 +1,89 @@
+import pyaudio
+import webrtcvad
+from constants import CHUNK_SIZE, FORMAT, RATE
+import os
+import wave
+
+class SoundHandler:
+
+    def __init__(self, stop_delay=100):
+        self.is_recording = False
+        self.data_buffer = []
+        self.vad = webrtcvad.Vad()
+        self.vad.set_mode(1)
+        self.p = None
+        self.stream = None
+        self.stop_delay = stop_delay
+        self.silent_count = 0
+
+        self.stream_out = None
+    
+    def _callback(self, in_data, frame_count, time_info, status):
+        if(self.is_recording):
+            self.data_buffer.append(in_data)
+            try:
+                if(self.vad.is_speech(in_data, RATE)):
+                    self.silent_count = 0
+                else:
+                    self.silent_count += 1
+
+            except:
+                print(in_data)
+                raise
+
+            if(self.silent_count > self.stop_delay):
+                self.is_recording = False
+                self.silent_count = 0
+        
+        return (in_data, pyaudio.paContinue)
+        
+    def start(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format=FORMAT,
+            channels=1,
+            rate=RATE,
+            input=True,
+            stream_callback=self._callback,
+            frames_per_buffer=CHUNK_SIZE
+        )
+        self.stream.start_stream()
+
+        self.stream_out = self.p.open(
+            format=FORMAT,
+            channels=1,
+            rate=RATE,
+            input=False,
+            output=True
+        )
+
+    def end(self):
+        if(self.stream != None and self.p != None and self.stream_out != None):
+            self.stream.stop_stream()
+            self.stream.close()
+            self.stream_out.stop_stream()
+            self.stream_out.close()
+            self.p.terminate()
+            self.p = None
+            self.stream = None
+            self.stream_out = None
+
+    def start_record(self):
+        if(not self.is_recording):
+            self.is_recording = True
+            print("Recording...")
+
+        while(self.is_recording):
+            pass
+
+        result = []
+        if(len(self.data_buffer) > 0):
+            result = self.data_buffer
+            self.data_buffer = []
+        
+        return result
+
+    def play_sound(self, filepath):
+        sound_wave = wave.open(filepath, 'rb')
+        data = sound_wave.readframes(sound_wave.getnframes())
+        self.stream_out.write(data)
